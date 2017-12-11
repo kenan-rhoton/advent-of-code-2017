b7da40f42aad5686fbad63404a5a025e2022b769
Kenan Rhoton <kenan.rhoton@gmail.com>
2017-12-11 12:03:45 +0100

    Day 5: Jumpcodes

---
+++ 05-C++/jump2.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <iostream>
+
+int main() {
+    std::vector<int> jumpcodes;
+    int num;
+    while (std::cin >> num) {
+        jumpcodes.push_back(num);
+    }
+    int position = 0;
+    int steps = 0;
+    while (position >= 0 && position < jumpcodes.size()) {
+        int val = jumpcodes[position];
+        if (val >= 3) {
+            jumpcodes[position]--;
+        } else {
+            jumpcodes[position]++;
+        }
+        steps++;
+        position += val;
+    }
+    std::cout << steps << std::endl;
+}
